0a1e4a73c77bcef403ce359b4f3746f4379f324a
jun-low <zhijun.low@gmail.com>
2019-12-22 19:02:31 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
         <p>
           This website indexes the top 10 cryptocurrencies by market cap (how
           much the sum of all coins is collectively worth), and gives you an easy
-          way to compare cryptocurrency performance and rank over the daily.
+          way to compare cryptocurrency performance and results every 10 seconds.
         </p>
       </div>
     </div>
