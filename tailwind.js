cd7d8672009ec9dcd437f544019c26b015fb9e42
jun-low <zhijun.low@gmail.com>
2019-12-22 18:57:38 +0800

    added tailwind

---
+++ tailwind.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem"
+    },
+    extend: {},
+  },
+  variants: {
+    tableLayout: ['responsive', 'hover', 'focus'],
+  },
+  plugins: [],
+};
